826de67454836448354de8315947ac8db3bd57c7
agathaferreiraa <agathafs0908@gmail.com>
2024-09-24 11:23:39 -0300

    numero aleatorio

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,11 +1,11 @@
 alert("Boas vindas ao jogo do número secreto");
-let numeroSecreto = 2;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 while (chute != numeroSecreto) {
-  chute = prompt("Escolha um número entre 1 e 10");
+  chute = prompt("Escolha um número entre 1 e 100");
 
   if (chute == numeroSecreto) {
     break;
@@ -18,5 +18,7 @@ while (chute != numeroSecreto) {
     tentativas++;
   }
 }
-let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(
+  `Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`
+);
